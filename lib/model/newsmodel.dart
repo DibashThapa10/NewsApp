e6a50d9f7a77db90194f7bc93658ac0e2db247bf
dibashthapa10 <dibashthapa447@gmail.com>
2022-07-17 18:05:53 +0545

    create model

---
+++ lib/model/newsmodel.dart
@@ -0,0 +1,14 @@
+class NewsModel {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+  NewsModel(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.body});
+  factory NewsModel.fromJson(Map<String, dynamic> json) {
+    return NewsModel(userId: json["userId"], id: json["id"], title: json["title"], body: json["body"]);
+  }
+}
