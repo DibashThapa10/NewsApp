65bd415b5577b3c63f1d5474f39bd2501d0c7c1e
dibashthapa10 <dibashthapa447@gmail.com>
2022-07-17 10:39:54 +0545

    add frame,heading and skip button in splash screen

--- lib/help_screen/splashscreen.dart
+++ lib/help_screen/splashscreen.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/homepage/homepage.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-        image: NetworkImage(
-            'https://www.vhv.rs/dpng/d/427-4270068_gold-retro-decorative-frame-png-free-download-transparent.png'),
-      ))),
+    double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/news_banner.png"),
+                ),
+              ),
+              child: Text(
+                'We show News For You',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Positioned(
+              top: 450,
+              left: 40,
+              right: 40,
+              child: SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Hompage()));
+                      },
+                      child: Text('Skip'))),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
